5b05f3e9a76013abb19eee27f1e51b83ae116945
aboussakkine-achraf <achrafaboussakkine@gmail.com>
2024-05-19 08:57:37 +0100

    mioplkeee

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 // app.use(cors({credentials:true,origin: 'https://emsipfe.vercel.app/'}));
 app.use(cors({
     credentials: true,
-    origin: 'https://emsipfe.vercel.app/',
+    origin: 'https://emsipfe.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE']
   }));
 
